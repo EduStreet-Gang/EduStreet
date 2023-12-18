f3176c7b2966fa7993d7a3795baa19db03cf28e1
rprasetya <rayaprasetya90@gmail.com>
2023-12-18 17:04:14 +0800

    ubah favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import Link from 'next/link'
 
 const poppins = Poppins({ 
   subsets: ['latin'],
